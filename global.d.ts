ec12b259b494699355dd0209342e102381c44979
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-27 16:45:38 -0800

    setting up components

---
+++ global.d.ts
@@ -0,0 +1,35 @@
+declare module '*.png' {
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.svg' {
+  import React from 'react';
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
+
+declare module '*.webp' {
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.avif' {
+  const value: ImageSourcePropType;
+  export default value;
+}
