6cb4f58267ea0594d669547c0ae7f0eea8d0a7be
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-27 22:01:42 -0800

    new getInitials function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getInitials(fullName?: string): string | undefined {
+  return !fullName
+    ? undefined
+    : fullName
+        .split(" ")
+        .filter(Boolean)
+        .map((word) => word[0]!.toUpperCase())
+        .join("");
+}
