372d5e851959c59c0131f29f7f81b754d5a348af
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-28 00:57:42 -0800

    create post modal

--- app/(nav)/create-post-modal.tsx
+++ app/(nav)/create-post-modal.tsx
@@ -1,115 +1,138 @@
-import * as React from "react";
 import { View } from "react-native";
-import Animated, {
-  FadeInUp,
-  FadeOutDown,
-  LayoutAnimationConfig,
-} from "react-native-reanimated";
-import { Info } from "~/lib/icons/Info";
-import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
-import { Button } from "~/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { Progress } from "~/components/ui/progress";
-import { Text } from "~/components/ui/text";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "~/components/ui/tooltip";
+import { router } from "expo-router";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { upload } from "cloudinary-react-native";
 
-const GITHUB_AVATAR_URI =
-  "https://i.pinimg.com/originals/ef/a2/8d/efa28d18a04e7fa40ed49eeb0ab660db.jpg";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Text } from "~/components/ui/text";
+import ImagePickerInput from "~/components/ImagePicker";
+
+import { useAuth } from "~/providers/auth.provider";
+import { CreatePostInput, PostService } from "~/services/post.service";
+import { fileToBase64 } from "~/lib/utils";
+
+const schema = z.object({
+  content: z.string().min(1, { message: "Escríbe algo." }),
+  file: z.any().optional(),
+});
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: process.env.EXPO_PUBLIC_CLOUDINARY_NAME
+  },
+  url: {
+    secure: true
+  }
+});
 
 export default function Screen() {
-  const [progress, setProgress] = React.useState(78);
+  const { token } = useAuth();
+  const { control, handleSubmit, formState } = useForm({
+    resolver: zodResolver(schema),
+  });
+  const { isLoading } = formState;
+  type SchemaType = z.infer<typeof schema>;
+  const postService = new PostService();
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: async (data: CreatePostInput) => {
+      const response = await postService.createPost(data);
+      if (response.error) {
+        throw new Error(response.error);
+      }
+      return response.data;
+    },
+    onSuccess: () => {
+      //@ts-ignore
+      queryClient.invalidateQueries(['posts', 'feed']);
+    },
+  });
 
-  function updateProgressValue() {
-    setProgress(Math.floor(Math.random() * 100));
-  }
+  const onSubmit = async (data: SchemaType) => {
+    try {
+      let public_id: string | undefined = undefined;
+
+      if(data.file) {
+        const base64Image = await fileToBase64(data.file);
+  
+        await upload(cld, { 
+          file: base64Image,
+           options: {
+            unsigned: true,
+           }, 
+           callback: (error, response) => {
+              if(error) {
+                console.error(error);
+              }
+    
+              public_id = response?.public_id!;
+            }
+          })
+
+        console.log(public_id);
+      }
+  
+  
+      mutate({
+        text: data.content,
+        media: public_id,
+        token,
+      });
+  
+      router.back();
+    } catch (error) {
+      console.error("aqui", error);
+    }
+  };
+
   return (
-    <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
-      <Card className="w-full max-w-sm p-6 rounded-2xl">
-        <CardHeader className="items-center">
-          <Avatar alt="Rick Sanchez's Avatar" className="w-24 h-24">
-            <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
-            <AvatarFallback>
-              <Text>RS</Text>
-            </AvatarFallback>
-          </Avatar>
-          <View className="p-3" />
-          <CardTitle className="pb-2 text-center">Rick Sanchez</CardTitle>
-          <View className="flex-row">
-            <CardDescription className="text-base font-semibold">
-              Scientist
-            </CardDescription>
-            <Tooltip delayDuration={150}>
-              <TooltipTrigger className="px-2 pb-0.5 active:opacity-50">
-                <Info
-                  size={14}
-                  strokeWidth={2.5}
-                  className="w-4 h-4 text-foreground/70"
-                />
-              </TooltipTrigger>
-              <TooltipContent className="py-2 px-4 shadow">
-                <Text className="native:text-lg">Freelance</Text>
-              </TooltipContent>
-            </Tooltip>
-          </View>
-        </CardHeader>
-        <CardContent>
-          <View className="flex-row justify-around gap-3">
-            <View className="items-center">
-              <Text className="text-sm text-muted-foreground">Dimension</Text>
-              <Text className="text-xl font-semibold">C-137</Text>
-            </View>
-            <View className="items-center">
-              <Text className="text-sm text-muted-foreground">Age</Text>
-              <Text className="text-xl font-semibold">70</Text>
-            </View>
-            <View className="items-center">
-              <Text className="text-sm text-muted-foreground">Species</Text>
-              <Text className="text-xl font-semibold">Human</Text>
-            </View>
-          </View>
-        </CardContent>
-        <CardFooter className="flex-col gap-3 pb-0">
-          <View className="flex-row items-center overflow-hidden">
-            <Text className="text-sm text-muted-foreground">Productivity:</Text>
-            <LayoutAnimationConfig skipEntering>
-              <Animated.View
-                key={progress}
-                entering={FadeInUp}
-                exiting={FadeOutDown}
-                className="w-11 items-center"
-              >
-                <Text className="text-sm font-bold text-sky-600">
-                  {progress}%
-                </Text>
-              </Animated.View>
-            </LayoutAnimationConfig>
-          </View>
-          <Progress
-            value={progress}
-            className="h-2"
-            indicatorClassName="bg-sky-600"
-          />
-          <View />
-          <Button
-            variant="outline"
-            className="shadow shadow-foreground/5"
-            onPress={updateProgressValue}
-          >
-            <Text>Update</Text>
-          </Button>
-        </CardFooter>
-      </Card>
+    <View className="flex-1 gap-5 px-4 pt-16 bg-secondary/30">
+      <Controller
+        control={control}
+        name="content"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <>
+            <Label nativeID="content">Contenido</Label>
+            <Input
+              nativeID="content"
+              placeholder="¿Qué tienes en mente?"
+              editable={!isLoading}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+            />
+          </>
+        )}
+      />
+      <Controller
+        control={control}
+        name="file"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <>
+            <Label nativeID="file">Imagen</Label>
+            <ImagePickerInput
+              value={value}
+              onBlur={onBlur}
+              onChange={onChange}
+              onCancel={() => onChange(undefined)}              
+            />
+          </>
+        )}
+      />
+      <Button
+        variant="primary"
+        className="w-full"
+        onPress={handleSubmit(onSubmit)}
+        disabled={isLoading}
+      >
+        <Text>Publicar</Text>
+      </Button>
     </View>
   );
 }
