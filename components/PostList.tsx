93bfe29b5babc07e3c3e8134c5a290c363c40d70
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-28 01:01:14 -0800

    minor fix post list

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,11 +1,11 @@
-import type { FC } from 'react'
-import { View, Text, VirtualizedList } from 'react-native'
+import React from 'react';
+import { View, Text, VirtualizedList } from 'react-native';
 import { FolderArchiveIcon } from 'lucide-react-native';
 
 import { Skeleton } from './ui/skeleton';
 import PostCard from './PostCard';
 
-import { Post } from '~/types/db'
+import { Post } from '~/types/db';
 
 interface PostListProps {
   data: Post[];
@@ -14,13 +14,13 @@ interface PostListProps {
   emptyMessage: string;
 }
 
-const PostList: FC<PostListProps> = ({ isFeed, isLoading, data, emptyMessage }) => {
-  if(isLoading) {
+const PostList: React.FC<PostListProps> = ({ isFeed, isLoading, data, emptyMessage }) => {
+  if (isLoading) {
     return (
-      <View className="flex-1 gap-4">
-        <Skeleton className="w-full h-44" />
-        <Skeleton className="w-full h-44" />
-        <Skeleton className="w-full h-44" />
+      <View style={{ flex: 1, gap: 16 }}>
+        <Skeleton className="w-full h-[176]" />
+        <Skeleton className="w-full h-[176]" />
+        <Skeleton className="w-full h-[176]" />
       </View>
     );
   }
@@ -30,18 +30,19 @@ const PostList: FC<PostListProps> = ({ isFeed, isLoading, data, emptyMessage }) 
       data={data}
       keyExtractor={(_, i) => i.toString()}
       getItemCount={(data: Post[]) => data.length}
-      getItem={(data: Post[], id: number) => data[id]}
+      getItem={(data: Post[], index: number) => data[index]}
       initialNumToRender={4}
-      renderItem={({item}) => <PostCard post={item} isFeed={isFeed} />}
-      contentContainerClassName="flex-1"
+      renderItem={({ item }) => <PostCard post={item} isFeed={isFeed} />}
+      contentContainerStyle={{ flexGrow: 1, paddingHorizontal: 16, gap: 16 }}
       ListEmptyComponent={() => (
-        <View className="flex-1 w-full justify-center items-center">
+        <View style={{ flex: 1, width: '100%', justifyContent: 'center', alignItems: 'center' }}>
           <FolderArchiveIcon color="grey" size={100} />
-          <Text className="font-semibold text-accent-foreground mt-4">{emptyMessage}</Text>
+          <Text style={{ fontWeight: '600', color: '#6b7280', marginTop: 16 }}>{emptyMessage}</Text>
         </View>
       )}
+      ListFooterComponent={<View style={{ marginBottom: 40 }} />}
     />
-  )
-}
+  );
+};
 
 export default PostList;
