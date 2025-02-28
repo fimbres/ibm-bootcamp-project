c4f638a38870582a0900f40c4598f602f5db6866
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-27 19:49:59 -0800

    formatting code

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,16 +1,18 @@
-import { Link, Stack } from 'expo-router';
-import { View } from 'react-native';
+import { Link, Stack } from "expo-router";
+import { View } from "react-native";
 
-import { Button } from '~/components/ui/button';
-import { Text } from '~/components/ui/text';
+import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: "Oops!" }} />
       <View>
-        <Text className='text-sm text-muted-foreground'>¡La Pantalla No Existe!</Text>
-        <Link href='/'>
+        <Text className="text-sm text-muted-foreground">
+          ¡La Pantalla No Existe!
+        </Text>
+        <Link href="/">
           <Button>Ir a Inicio</Button>
         </Link>
       </View>
