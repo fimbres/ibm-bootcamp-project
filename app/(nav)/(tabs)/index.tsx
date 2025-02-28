621fd58e7c289ef4846659eb341a42f5d6a9ff5b
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-27 21:59:54 -0800

    new home page

--- app/(nav)/(tabs)/index.tsx
+++ app/(nav)/(tabs)/index.tsx
@@ -1,115 +1,51 @@
-import * as React from "react";
 import { View } from "react-native";
-import Animated, {
-  FadeInUp,
-  FadeOutDown,
-  LayoutAnimationConfig,
-} from "react-native-reanimated";
-import { Info } from "~/lib/icons/Info";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { router } from "expo-router";
+
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
-import { Button } from "~/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { Progress } from "~/components/ui/progress";
+import { Input } from "~/components/ui/input";
 import { Text } from "~/components/ui/text";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "~/components/ui/tooltip";
+import PostList from "~/components/PostList";
+
+import { useAuth } from "~/providers/auth.provider";
+import { PostService } from "~/services/post.service";
+import { getInitials } from "~/lib/utils";
 
 const GITHUB_AVATAR_URI =
-  "https://i.pinimg.com/originals/ef/a2/8d/efa28d18a04e7fa40ed49eeb0ab660db.jpg";
+"https://i.pinimg.com/originals/ef/a2/8d/efa28d18a04e7fa40ed49eeb0ab660db.jpg";
 
 export default function Screen() {
-  const [progress, setProgress] = React.useState(78);
+  const postService = new PostService();
+  const { token, user } = useAuth();
+  const { error, data, isLoading } = useSuspenseQuery({
+    queryKey: ["feed", token],
+    queryFn: async () => {
+      const response = await postService.getFeed(token!);
+      
+      if (response.error) {
+        throw new Error(response.error);
+      }
+      return response.data || [];
+    }
+  });
 
-  function updateProgressValue() {
-    setProgress(Math.floor(Math.random() * 100));
-  }
   return (
-    <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
-      <Card className="w-full max-w-sm p-6 rounded-2xl">
-        <CardHeader className="items-center">
-          <Avatar alt="Rick Sanchez's Avatar" className="w-24 h-24">
-            <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
-            <AvatarFallback>
-              <Text>RS</Text>
-            </AvatarFallback>
-          </Avatar>
-          <View className="p-3" />
-          <CardTitle className="pb-2 text-center">Rick Sanchez</CardTitle>
-          <View className="flex-row">
-            <CardDescription className="text-base font-semibold">
-              Scientist
-            </CardDescription>
-            <Tooltip delayDuration={150}>
-              <TooltipTrigger className="px-2 pb-0.5 active:opacity-50">
-                <Info
-                  size={14}
-                  strokeWidth={2.5}
-                  className="w-4 h-4 text-foreground/70"
-                />
-              </TooltipTrigger>
-              <TooltipContent className="py-2 px-4 shadow">
-                <Text className="native:text-lg">Freelance</Text>
-              </TooltipContent>
-            </Tooltip>
-          </View>
-        </CardHeader>
-        <CardContent>
-          <View className="flex-row justify-around gap-3">
-            <View className="items-center">
-              <Text className="text-sm text-muted-foreground">Dimension</Text>
-              <Text className="text-xl font-semibold">C-137</Text>
-            </View>
-            <View className="items-center">
-              <Text className="text-sm text-muted-foreground">Age</Text>
-              <Text className="text-xl font-semibold">70</Text>
-            </View>
-            <View className="items-center">
-              <Text className="text-sm text-muted-foreground">Species</Text>
-              <Text className="text-xl font-semibold">Human</Text>
-            </View>
-          </View>
-        </CardContent>
-        <CardFooter className="flex-col gap-3 pb-0">
-          <View className="flex-row items-center overflow-hidden">
-            <Text className="text-sm text-muted-foreground">Productivity:</Text>
-            <LayoutAnimationConfig skipEntering>
-              <Animated.View
-                key={progress}
-                entering={FadeInUp}
-                exiting={FadeOutDown}
-                className="w-11 items-center"
-              >
-                <Text className="text-sm font-bold text-sky-600">
-                  {progress}%
-                </Text>
-              </Animated.View>
-            </LayoutAnimationConfig>
-          </View>
-          <Progress
-            value={progress}
-            className="h-2"
-            indicatorClassName="bg-sky-600"
-          />
-          <View />
-          <Button
-            variant="outline"
-            className="shadow shadow-foreground/5"
-            onPress={updateProgressValue}
-          >
-            <Text>Update</Text>
-          </Button>
-        </CardFooter>
-      </Card>
+    <View className="flex-1 gap-5 p-4 bg-secondary/30">
+      <View className="flex flex-row gap-4 w-full items-center">
+        <Avatar alt={`User ${user?.name}`} className="w-14 h-14">
+          <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
+          <AvatarFallback>
+            <Text>{getInitials(user?.name)}</Text>
+          </AvatarFallback>
+        </Avatar>
+        <Input className="flex-1" placeholder="Escribe algo..." editable={false} onPress={() => router.push("/(nav)/create-post-modal")} />
+      </View>
+      <PostList
+        isLoading={isLoading}
+        isFeed={true}
+        data={data}
+        emptyMessage="No hay publicaciones para ver."
+      />
     </View>
   );
 }
